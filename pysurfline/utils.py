2e8dfa291712adadf48f77bf3ec18bb3217ad01f
giocaizzi <giocaizzi@gmail.com>
2022-02-12 17:35:14 +0100

    get_dataframe

---
+++ pysurfline/utils.py
@@ -0,0 +1,22 @@
+"""
+utility functions
+"""
+
+import collections
+
+def flatten(d, parent_key='', sep='_'):
+    """
+    flatten nested dictionary, preserving lists
+
+    Arguments:
+        parent_key (str):
+        sep (str):
+    """
+    items = []
+    for k, v in d.items():
+        new_key = parent_key + sep + k if parent_key else k
+        if v and isinstance(v, collections.MutableMapping):
+            items.extend(flatten(v, new_key, sep=sep).items())
+        else:
+            items.append((new_key, v))
+    return dict(items)
