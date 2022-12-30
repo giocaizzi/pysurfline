8ef99404bb70156f4c02f235c0467bcd7862d036
giocaizzi <giocaizzi@gmail.com>
2022-12-30 18:50:33 +0100

    tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -2,3 +2,14 @@
 test core classes.
 """
 import pytest
+
+from pysurfline import ForecastGetter
+
+SPOTID="5842041f4e65fad6a7708890"
+
+
+def test_ForecastGetter():
+    f=ForecastGetter("wave",params={"SpotId":SPOTID})
+    assert f.type == "wave"
+    assert f.response.url == "https://services.surfline.com/kbyg/spots/forecasts/"\
+        "wave?SpotId=5842041f4e65fad6a7708890"
