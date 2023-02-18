723a1ba616af3a76caff7902eb5bf9ed2b1d4d39
giocaizzi <giocaizzi@gmail.com>
2023-02-18 16:38:17 +0100

    tests SurflineAPI

--- tests/test_core.py
+++ tests/test_core.py
@@ -8,12 +8,14 @@ from pysurfline.core import SurflineAPI
 def api():
     return SurflineAPI("123")
 
+
 def test_init(api):
     """
     Test the initialization of the API object.
     """
     assert api.endpoint == "https://services.surfline.com/kbyg/spots/forecasts?"
     assert api.spot_id == "123"
+
 
 @mock.patch("pysurfline.core.requests.get")
 def test_get_forecast(mock_get, api):
@@ -22,7 +24,7 @@ def test_get_forecast(mock_get, api):
     """
     mock_get.return_value.status_code = 200
     mock_get.return_value.json.return_value = {"foo": "bar"}
-    
+
     # dafault parameters
     assert api.get_forecast() == {"foo": "bar"}
     mock_get.assert_called_once_with(
@@ -37,13 +39,21 @@ def test_get_forecast(mock_get, api):
         params={"spotId": "123", "days": 2, "type": "wind", "interval_hours": 6},
     )
 
+
 @mock.patch("pysurfline.core.requests.get")
-def test_get_forecast_error( mock_get, api):
+def test_get_forecast_error(mock_get, api):
     """
     Test the retrieval of forecast data from the API with an error status code.
     """
     mock_get.return_value.raise_for_status.side_effect = requests.exceptions.HTTPError()
 
-
     with pytest.raises(requests.exceptions.HTTPError):
-        response = api.get_forecast()
+        api.get_forecast()
+
+
+@mock.patch("pysurfline.core.requests.get")
+def test_get_forecast_raises_othererror(mock_get, api):
+    mock_get.return_value.raise_for_status.side_effect = ValueError()
+
+    with pytest.raises(ValueError):
+        api.get_forecast()
