229005bdadd335623bf976bf6a6f8cf4e7433f0e
giocaizzi <giocaizzi@gmail.com>
2023-02-19 01:52:33 +0100

    add_now_line

--- tests/test_reports.py
+++ tests/test_reports.py
@@ -60,6 +60,26 @@ def test_add_grid(patched_SpotForecast):
         assert line.get_color() == "k"
 
 
+def test_add_now_line(patched_SpotForecast):
+    """test that a red line is placed at the now date"""
+    r = SurfReport(patched_SpotForecast)
+    now = matplotlib.dates.date2num(
+        patched_SpotForecast.sunriseSunsetTimes["midnight"][0]
+        + datetime.timedelta(hours=4)
+    )
+    fig, ax = plt.subplots()
+    with mock.patch("pysurfline.reports.datetime.datetime") as mock_now:
+        mock_now.now.return_value = now
+        r._add_now_line(ax)
+    # Check that the line exists and is in the correct position
+    lines = ax.get_lines()
+    assert len(lines) == 1
+    assert lines[0].get_color() == "r"
+    assert lines[0].get_linewidth() == 0.5
+    assert lines[0].get_label() == "Now"
+    assert lines[0].get_xdata() == pytest.approx([now, now], abs=1e-2)
+
+
 def test_add_day_night(patched_SpotForecast):
     sunrise_sunset_times = patched_SpotForecast.sunriseSunsetTimes
     sunrise_sunset_times["endoftheday"] = sunrise_sunset_times[
@@ -72,10 +92,11 @@ def test_add_day_night(patched_SpotForecast):
         .reset_index(drop=True)
     )
     expected_spans = [
-            (expected[i],expected[i + 1]) for i in range(len(expected)) if i % 2 == 0
-        ]
-    expected_colors = [(0.6627450980392157, 0.6627450980392157, 0.6627450980392157, 1.0)]
- 
+        (expected[i], expected[i + 1]) for i in range(len(expected)) if i % 2 == 0
+    ]
+    expected_colors = [
+        (0.6627450980392157, 0.6627450980392157, 0.6627450980392157, 1.0)
+    ]
 
     r = SurfReport(patched_SpotForecast)
     fig, ax = plt.subplots()
@@ -88,16 +109,15 @@ def test_add_day_night(patched_SpotForecast):
     assert len(actual_patches) == len(expected_spans)
 
     for patch, expected_span, expected_color in zip(
-        actual_patches,expected_spans,expected_colors*len(expected_spans)
+        actual_patches, expected_spans, expected_colors * len(expected_spans)
     ):
         assert patch.get_facecolor() == expected_color
-        es1=matplotlib.dates.date2num(expected_span[0])
-        es2=matplotlib.dates.date2num(expected_span[1])
+        es1 = matplotlib.dates.date2num(expected_span[0])
+        es2 = matplotlib.dates.date2num(expected_span[1])
         as1 = patch.get_xy()[0, 0]
         as2 = patch.get_xy()[3, 0]
 
-        print(as1,as2, es1,es2)
+        print(as1, as2, es1, es2)
 
         assert as1 == es1
         assert as2 == es2
-
