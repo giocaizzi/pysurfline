fac3483aba4a7989ef46f33e199f3287b51a1509
giocaizzi <giocaizzi@gmail.com>
2023-10-04 19:59:53 +0200

    params

--- pysurfline/__init__.py
+++ pysurfline/__init__.py
@@ -2,7 +2,7 @@
 python Surfline API
 """
 # Version
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 # Credits
 __author__ = "Giorgio Caizzi"
--- pysurfline/api.py
+++ pysurfline/api.py
@@ -5,7 +5,7 @@ import requests
 from .models import SpotForecasts
 
 
-def get_spot_forecasts(spotId: str) -> SpotForecasts:
+def get_spot_forecasts(spotId: str,**kwargs) -> SpotForecasts:
     """get spot forecast
 
     Get forecast for given spot by passing the spotId
@@ -17,7 +17,7 @@ def get_spot_forecasts(spotId: str) -> SpotForecasts:
     Returns:
         forecast (:obj:`SpotForecast`)
     """
-    return SurflineClient()._get_spot_forecasts(spotId)
+    return SurflineClient()._get_spot_forecasts(spotId,**kwargs)
 
 
 class SurflineClient:
@@ -32,22 +32,24 @@ class SurflineClient:
     def __init__(self):
         pass
 
-    def _get_spot_forecasts(self, spotId: str) -> SpotForecasts:
+    def _get_spot_forecasts(self, spotId: str, **kwargs) -> SpotForecasts:
         """create a SpotForecast object from API responses
 
         Arguments:
             spotId (str): spot id
+            \\*\\*kwargs: keyword arguments to spot/forecasts endpoint
 
         Returns:
             SpotForecast: SpotForecast object
         """
+        kwargs["spotId"] = spotId
         return SpotForecasts(
             spotId,
             **APIResource(self, "spots/details")
             .get(params={"spotId": spotId})
             .json["spot"],
             **APIResource(self, "spots/forecasts")
-            .get(params={"spotId": spotId})
+            .get(params=kwargs)
             .json["data"],
         )
 
